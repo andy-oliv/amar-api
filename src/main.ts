685bdf19ab29eb4e8fcac0125b10c1db17e69bad
Andrew Pacheco <andrew.pacheco@outlook.com>
2024-12-11 10:18:42 -0300

    swagger customization

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,51 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger)); //Pino configuration
   app.useGlobalPipes(new ValidationPipe()); //class-validator and class-transformer configuration
   app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(
+    '/logo.png',
+    express.static(join(__dirname, '..', 'src', 'assets', 'logo.png')),
+  );
+  app.use(
+    '/favicon.ico',
+    express.static(join(__dirname, '..', 'src', 'assets', 'favicon.ico')),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Amar Infâncias API')
-    .setDescription("API for Amar Infâncias' admin panel web app.")
+    .setDescription(
+      "RESTful API for managing Amar Infâncias' data and processes.",
+    )
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+
+  const swaggerOptions = {
+    customSiteTitle: 'Amar Infâncias API Docs',
+    customfavIcon: '/favicon.ico',
+    customCss: `
+      .swagger-ui .topbar { background-color: #5951a2; }
+      .swagger-ui .topbar .topbar-wrapper::before {
+        content: '';
+        background-image: url('/logo.png'); 
+        background-repeat: no-repeat;
+        background-size: contain;
+        height: 50px; 
+        width: 150px;
+        display: inline-block;
+        margin-right: 10px;
+      }
+    `,
+  };
+
+  SwaggerModule.setup('api', app, documentFactory, swaggerOptions);
 
   await app.listen(3000);
 }
